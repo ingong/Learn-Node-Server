e2b0655e6600a34f5292f52f097d6c7df1f36c48
ingong <insong1216@gmail.com>
2021-09-20 01:34:05 +0900

    add: main.js 에 db 에 의한 데이터 콘솔 확인
    기존 파일형태로 읽어오는 부분을 주석처리하고
    mysql 에서 해당 db 에서 데이터를 가져오게 수정

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -12,8 +12,38 @@ connection.connect();
 
 connection.query('SELECT * FROM topic', function (error, results, fields) {
   if (error) throw error;
-  // console.log(process.env);
   console.log(results);
 });
 
 connection.end();
+
+// const dotenv = require('dotenv');
+// const db = require('mysql2/promise');
+// dotenv.config({ path: './.env' });
+// let database = {};
+
+// database.init = async app => {
+//   const pool = db.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USER,
+//     password: process.env.DB_PWD,
+//     database: process.env.DB_NAME,
+//   });
+
+//   database.pool = pool;
+//   app.set('database', database);
+//   const dbTest = app.get('database');
+//   const result = await test(dbTest);
+//   console.log(result);
+// };
+
+// const test = async db => {
+//   const conn = await db.pool.getConnection(async conn => conn);
+//   const result = await conn.query('SELECT * FROM topic', function (error, results, fields) {
+//     return results;
+//   });
+//   conn.release();
+//   return result;
+// };
+
+// module.exports = database;
