2bb78a9f27a90d69368895815d3673ad446bc4d2
ingong <insong1216@gmail.com>
2021-09-20 01:12:02 +0900

    feat: .env 파일 환경변수 추가 및 생성
    access denied 문제로 MySQL 재설치 및 권한 부여했음
    거의 2시간은 걸린 것 같다... 이유는 아직 명확히 파악못함
    user 를 만들고, 권한을 부여하고, .env 파일 경로 확인 후 해결

--- nodejs/mysql.js
+++ nodejs/mysql.js
@@ -1,16 +1,18 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config({ path: './.env' });
 const db = require('mysql2');
 const connection = db.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '*dlsthd12',
-  database: 'opentutorials',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PWD,
+  database: process.env.DB_NAME,
 });
 
 connection.connect();
 
 connection.query('SELECT * FROM topic', function (error, results, fields) {
   if (error) throw error;
+  // console.log(process.env);
   console.log(results);
 });
 
